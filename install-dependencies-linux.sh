5742c02032f246632b645b1c8a19fc3fd502f256
Kenneth Lausdahl <kenneth@lausdahl.com>
2025-01-10 15:57:56 +0100

    added helper script

---
+++ install-dependencies-linux.sh
@@ -0,0 +1,1 @@
+apt install unzip  nano build-essential cmake git zlib1g-dev python3 bzip2  lbzip2 zlib1g-dev libbz2-dev libssl-dev liblzma-dev libzstd-dev libssl-dev python3.12-venv libmbedtls-dev pkg-config -y

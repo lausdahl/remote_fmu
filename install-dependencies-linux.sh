c77bc72e01279a5f6da5f50bd0852af379299899
Kenneth Lausdahl <kenneth@lausdahl.com>
2025-01-13 09:46:57 +0100

    updated build script

--- install-dependencies-linux.sh
+++ install-dependencies-linux.sh
@@ -1,3 +1,3 @@
 export DEBIAN_FRONTEND=noninteractive
 
-apt install unzip  nano build-essential cmake git zlib1g-dev python3 bzip2  lbzip2 zlib1g-dev libbz2-dev libssl-dev liblzma-dev libzstd-dev libssl-dev python3.12-venv libmbedtls-dev pkg-config -y
+apt-get install unzip  nano build-essential cmake git zlib1g-dev python3 bzip2  lbzip2 zlib1g-dev libbz2-dev libssl-dev liblzma-dev libzstd-dev libssl-dev python3.12-venv libmbedtls-dev pkg-config -y
